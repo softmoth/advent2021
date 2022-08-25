5220b32dfeddf9ceb805b4628ffc5a9f96524a5a
Tim Siegel <siegeltr@gmail.com>
2022-08-24 22:28:59 -0400

    day06: Part 2 + a bit more

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -4,7 +4,7 @@
 #![warn(clippy::nursery)]
 //#![warn(clippy::cargo)]
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 //use ndarray::prelude::*;
 use nom::{bytes::complete::tag, character::complete::u8, multi::separated_list1, IResult};
 
@@ -32,7 +32,7 @@ fn finish<A>(parsed: IResult<&str, A>) -> Result<A> {
 }
 
 // Hope there's never more than this many fish with any given timer value
-type FishCount = u32;
+type FishCount = u64;
 // Fish timer is between 0 and 8
 const TIMER_END: usize = 9;
 
@@ -46,35 +46,44 @@ struct School {
 impl School {
     fn new(fishes: Vec<u8>) -> Self {
         let mut counts = SchoolStorage::default();
+
         for timer in fishes {
             let timer = usize::from(timer);
-            if timer >= TIMER_END {
-                panic!("Invalid input, fish timer too large: {}", timer);
-            }
+            assert!(
+                timer < TIMER_END,
+                "Invalid input, fish timer too large: {}",
+                timer
+            );
             counts[timer] += 1;
         }
-        School {
+
+        Self {
             counts,
             zero_index: 0,
         }
     }
 
-    fn advance(&mut self) {
+    fn advance(&mut self) -> Result<()> {
         let of_day = |i| (self.zero_index + i) % TIMER_END;
 
         // Keep the zero count as is; it will become the new fish with timer 8
         // self.counts[of_day((TIMER_END - 1) + 1)] = self.counts[self.zero_index];
 
-        // And also add the zero count to day 6
-        self.counts[of_day(6 + 1)] += self.counts[self.zero_index];
+        // And also add the zero count to (the upcoming) day 6
+        self.counts[of_day(6 + 1)] = self.counts[of_day(6 + 1)]
+            .checked_add(self.counts[self.zero_index])
+            .ok_or_else(|| anyhow!("Counter overflowed, need more bits!"))
+            .context("Advancing a new generation")?;
 
         // It's now the next day; 1 -> 0, ..., 8 -> 7, 0 -> 8.
         self.zero_index = of_day(1);
+
+        Ok(())
     }
 }
 
 /*
-Part 1:
+Part 1 (80 days):
 Answer: 375482
 dhat: Total:     2,657 bytes in 10 blocks
 dhat: At t-gmax: 1,624 bytes in 2 blocks
@@ -83,14 +92,38 @@ dhat: The data has been saved to dhat-heap.json, and is viewable with dhat/dh_vi
 0.00user 0.00system 0:00.00elapsed 100%CPU (0avgtext+0avgdata 10440maxresident)k
 0inputs+48outputs (0major+831minor)pagefaults 0swaps
 
+Part 2 (256 days):
+Answer: 1689540415957
+dhat: Total:     2,657 bytes in 10 blocks
+dhat: At t-gmax: 1,624 bytes in 2 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data has been saved to dhat-heap.json, and is viewable with dhat/dh_view.html
+0.00user 0.00system 0:00.00elapsed 100%CPU (0avgtext+0avgdata 10728maxresident)k
+0inputs+16outputs (0major+837minor)pagefaults 0swaps
+
+Part 3 (Max days that will fit into a u64 final result = 442):
+Answer: 18353315898976047013
+dhat: Total:     2,657 bytes in 10 blocks
+dhat: At t-gmax: 1,624 bytes in 2 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data has been saved to dhat-heap.json, and is viewable with dhat/dh_view.html
+0.00user 0.00system 0:00.00elapsed 80%CPU (0avgtext+0avgdata 10464maxresident)k
+0inputs+16outputs (0major+828minor)pagefaults 0swaps
+
 */
 
 fn process(input: &str) -> Result<u64> {
     let mut school = School::new(finish(separated_list1(tag(","), u8)(input))?);
-    for _day in 0..80 {
+    for _day in 0..256 {
         //dbg!(&school);
-        school.advance();
+        school.advance()?;
     }
     dbg!(&school);
-    Ok(school.counts.iter().map(|v| u64::from(*v)).sum())
+    // NB: .sum() isn't checked, so implement it via a fold
+    school
+        .counts
+        .iter()
+        .try_fold(0u64, |acc, v| acc.checked_add(*v))
+        .ok_or_else(|| anyhow!("Counter overflowed, need more bits!"))
+        .context("Final sum")
 }
